dc8324eb7c984a27cfdfe0b72c84ab41a9488485
shilvan <5.shilvandacosta@gmail.com>
2020-08-28 13:55:05 +0100

    change the pop up content depending on the project clicked

--- flask/app/static/js/portfolio.js
+++ flask/app/static/js/portfolio.js
@@ -4,18 +4,113 @@ var main = function(){
 
 	$("#close-work").on("click", function(){
 		$(".work-display-wrapper").css("display", "none");
+		$(".tech-used").empty();
 
 
 	});
 
 	$(".project-link").on("click", function(){
-		$(".work-display-wrapper").css("display", "grid");
-
-
+		
 	});
 
 
-	
+	$("#appointment").on("click", function(){
+		$(".work-display-wrapper").css("display", "grid");
+
+		$("#title").text("Appointment");
+		$("#description").text("A website that allows clients to book appointments, and employees to manage them.");
+		$(".tech-used").append('<li>html</li>');
+		$(".tech-used").append('<li>css</li>');
+		$(".tech-used").append('<li>js</li>');
+		$(".tech-used").append('<li>flask</li>');
+		$(".tech-used").append('<li>python</li>');
+		$(".tech-used").append('<li>postgres</li>');
+		$(".tech-used").append('<li>vagrant</li>');
+		$(".tech-used").append('<li>docker</li>');
+		$(".tech-used").append('<li>git</li>');
+		$("#more-details").text("More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything");
+
+		$("#demo-btn").css("display", "inline-block");
+		/*$("#demo-btn").attr("href", "{{ url_for('cuttem') }}");*/
+		$("#demo-btn").on("click", function(){
+			location.assign("/appointment");
+		});
+
+		$("#code-btn").css("display", "inline-block");
+		$("#code-btn").attr("href", "https://www.github.com/shilvan");
+
+	});
+
+	$("#vaxoo").on("click", function(){
+		$(".work-display-wrapper").css("display", "grid");
+
+		$("#title").text("Vaxoo");
+		$("#description").text("E-commerce website");
+		$(".tech-used").append('<li>wordpress</li>');
+		$(".tech-used").append('<li>woo-commerce</li>');
+		$(".tech-used").append('<li>stripe api</li>');
+		
+		$("#more-details").text("More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything");
+
+		$("#demo-btn").css("display", "inline-block");
+		/*$("#demo-btn").attr("href", "{{ url_for('cuttem') }}");*/
+		$("#demo-btn").on("click", function(){
+			location.assign("https://staging.vaxoo.co.uk/");
+		});
+
+		$("#code-btn").css("display", "none");
+		$("#code-btn").attr("href", "https://www.github.com/shilvan");
+
+	});
+
+	$("#calculator").on("click", function(){
+		$(".work-display-wrapper").css("display", "grid");
+		
+		$("#title").text("C# Calculator");
+		$("#description").text("An app that uses regex for string manipulation and coded mathematical order and operations to calculate user entries");
+		$(".tech-used").append('<li>c#</li>');
+		$(".tech-used").append('<li>regex</li>');
+		
+		$("#more-details").text("More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything");
+
+		$("#demo-btn").css("display", "none");
+		/*$("#demo-btn").attr("href", "{{ url_for('cuttem') }}");*/
+		$("#demo-btn").on("click", function(){
+			location.assign("/lina");
+		});
+
+		$("#code-btn").css("display", "inline-block");
+		$("#code-btn").attr("href", "https://www.github.com/shilvan");
+
+	});
+
+	$("#portfolio").on("click", function(){
+		$(".work-display-wrapper").css("display", "grid");
+		
+		$("#title").text("My Portfolio");
+		$("#description").text("My portfolio website to display my projects");
+		$(".tech-used").append('<li>html</li>');
+		$(".tech-used").append('<li>css</li>');
+		$(".tech-used").append('<li>js</li>');
+		$(".tech-used").append('<li>flask</li>');
+		$(".tech-used").append('<li>python</li>');
+		$(".tech-used").append('<li>postgres</li>');
+		$(".tech-used").append('<li>vagrant</li>');
+		$(".tech-used").append('<li>docker</li>');
+		$(".tech-used").append('<li>git</li>');
+		
+		$("#more-details").text("More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything");
+
+		$("#demo-btn").css("display", "inline-block");
+		/*$("#demo-btn").attr("href", "{{ url_for('cuttem') }}");*/
+		$("#demo-btn").on("click", function(){
+			location.assign("/");
+		});
+
+		$("#code-btn").css("display", "inline-block");
+		$("#code-btn").attr("href", "https://www.github.com/shilvan");
+
+	});
 
 };
 
